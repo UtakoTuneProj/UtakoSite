2403e1f89f81833c94a5a5f163d18b598577b4c2
Nanamachi <7machi@nanamachi.net>
2018-03-19 05:30:49 +0900

    Django initial Commit

---
+++ models/chart.py
@@ -0,0 +1,21 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+class Chart(models.Model):
+    id = models.CharField(db_column='ID', primary_key=True, max_length=12)  # Field name made lowercase.
+    epoch = models.IntegerField()
+    time = models.FloatField(db_column='Time')  # Field name made lowercase.
+    view = models.IntegerField(db_column='View')  # Field name made lowercase.
+    comment = models.IntegerField(db_column='Comment')  # Field name made lowercase.
+    mylist = models.IntegerField(db_column='Mylist')  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'chart'
+        unique_together = (('id', 'epoch'),)
+
---
+++ models/status.py
@@ -0,0 +1,21 @@
+
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+class Status(models.Model):
+    id = models.CharField(db_column='ID', primary_key=True, max_length=12)  # Field name made lowercase.
+    validity = models.IntegerField()
+    epoch = models.IntegerField()
+    iscomplete = models.IntegerField(db_column='isComplete')  # Field name made lowercase.
+    postdate = models.DateTimeField(blank=True, null=True)
+    analyzegroup = models.IntegerField(db_column='analyzeGroup', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'status'
+

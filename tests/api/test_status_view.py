cf0fd019f699a450d3d2d004e9d5f0ff34e8896a
Nanamachi <7machi@nanamachi.net>
2018-11-16 19:26:28 +0900

    add tests for api view

--- tests/api/test_status_view.py
+++ tests/api/test_status_view.py
@@ -1,12 +1,12 @@
+from datetime import timedelta, datetime
 import pytest
-import math
-from datetime import datetime, timedelta
+import json
 from django.test import Client
 
 from tests.helpers import StatusCreationHelper as sch
 
 @pytest.mark.django_db
-class TestMovieViewIndex():
+class TestMovieViewIndex:
     c = Client()
     now = datetime.now()
 
@@ -32,47 +32,12 @@ class TestMovieViewIndex():
             max_view=90,
             tags=['にこにこ', '初音ミク'],
         ))
-        response = self.c.get('/movie/')
+        response = self.c.get('/api/movie/')
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # not return non-indexed movie
-        assert response.context['page_obj'].paginator.count == 2
-
-    def test_page(self):
-        sch().create_testcases(dict(
-            mvid='sm1',
-            postdate=self.now - timedelta(days=8),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,0,0,0,0,0,0,0),
-        ),dict(
-            mvid='sm2',
-            postdate=self.now - timedelta(days=7),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,1,0,0,0,0,0,0),
-        ),dict(
-            mvid='sm3',
-            postdate=self.now - timedelta(days=6),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,0,1,0,0,0,0,0),
-        ),dict(
-            mvid='sm4',
-            postdate=self.now - timedelta(days=5),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,0,0,1,0,0,0,0),
-        ))
-        response = self.c.get('/movie/', {'page': 2, 'perpage':3})
-        # status must be OK
-        assert response.status_code == 200
-        # returns offset 3
-        assert len(response.context['page_obj'].object_list) == 1
+        assert result['count'] == 2
 
     def test_sortby_postdate(self):
         sch().create_testcases(dict(
@@ -104,11 +69,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', '初音ミク'],
             index=(1,0,0,1,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'sortby': 'postdate'})
+        response = self.c.get('/api/movie/', {'sortby': 'postdate'})
+        result = json.loads(response.content.decode())
         # status must be ok
         assert response.status_code == 200
         # ordered by postdate asc
-        assert list(map(lambda x: getattr(x, 'id'), response.context['page_obj'].object_list )) == ['sm1', 'sm2', 'sm3', 'sm4']
+        assert list(map(lambda x: x['id'], result['results'] )) == ['sm1', 'sm2', 'sm3', 'sm4']
 
     def test_sortby_postdate_desc(self):
         sch().create_testcases(dict(
@@ -140,11 +106,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', '初音ミク'],
             index=(1,0,0,1,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'sortby': '-postdate'})
+        response = self.c.get('/api/movie/', {'sortby': '-postdate'})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # ordered by postdate desc
-        assert list(map(lambda x: getattr(x, 'id'), response.context['page_obj'].object_list )) == ['sm4', 'sm3', 'sm2', 'sm1']
+        assert list(map(lambda x: x['id'], result['results'] )) == ['sm4', 'sm3', 'sm2', 'sm1']
 
     def test_sortby_maxview_asc(self):
         sch().create_testcases(dict(
@@ -176,11 +143,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', '初音ミク'],
             index=(1,0,0,1,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'sortby': 'max_view'})
+        response = self.c.get('/api/movie/', {'sortby': 'max_view'})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # ordered by max_view asc
-        assert list(map(lambda x: getattr(x, 'id'), response.context['page_obj'].object_list )) == ['sm3', 'sm4', 'sm1', 'sm2']
+        assert list(map(lambda x: x['id'], result['results']  )) == ['sm3', 'sm4', 'sm1', 'sm2']
 
     def test_sortby_maxview_desc(self):
         sch().create_testcases(dict(
@@ -212,11 +180,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', '初音ミク'],
             index=(1,0,0,1,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'sortby': '-max_view'})
+        response = self.c.get('/api/movie/', {'sortby': '-max_view'})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # ordered by max_view desc
-        assert list(map(lambda x: getattr(x, 'id'), response.context['page_obj'].object_list )) == ['sm2', 'sm1', 'sm4', 'sm3']
+        assert list(map(lambda x: x['id'], result['results'] )) == ['sm2', 'sm1', 'sm4', 'sm3']
 
     def test_not_analyzed(self):
         sch().create_testcases(dict(
@@ -246,11 +215,12 @@ class TestMovieViewIndex():
             max_view=90,
             tags=['にこにこ', '初音ミク'],
         ))
-        response = self.c.get('/movie/', {'not_analyzed': 'on'})
+        response = self.c.get('/api/movie/', {'not_analyzed': 'on'})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # returns all tracked movie even if not analyzed
-        assert response.context['page_obj'].paginator.count == 4
+        assert result['count'] == 4
 
     def test_min_view(self):
         sch().create_testcases(dict(
@@ -282,11 +252,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', '初音ミク'],
             index=(1,0,1,0,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'min_view': 100})
+        response = self.c.get('/api/movie/', {'min_view': 100})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # returns max_view >= 100
-        assert response.context['page_obj'].paginator.count == 3
+        assert result['count'] == 3
 
     def test_max_view(self):
         sch().create_testcases(dict(
@@ -318,11 +289,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', '初音ミク'],
             index=(1,0,1,0,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'max_view': 100})
+        response = self.c.get('/api/movie/', {'max_view': 100})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # returns max_view <= 100
-        assert response.context['page_obj'].paginator.count == 3
+        assert result['count'] == 3
 
     def test_tags(self):
         sch().create_testcases(dict(
@@ -354,11 +326,12 @@ class TestMovieViewIndex():
             tags=['にこにこ', 'GUMI'],
             index=(1,0,1,0,0,0,0,0),
         ))
-        response = self.c.get('/movie/', {'tags': '初音ミク'})
+        response = self.c.get('/api/movie/', {'tags': '初音ミク'})
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # returns max_view <= 100
-        assert response.context['page_obj'].paginator.count == 3
+        assert result['count'] == 3
 
 @pytest.mark.django_db
 class TestMovieViewDetail:
@@ -374,68 +347,13 @@ class TestMovieViewDetail:
             tags=['にこにこ', '初音ミク'],
             index=(1,0,0,0,0,0,0,0),
         ))
-        response = self.c.get('/movie/sm1/')
+        response = self.c.get('/api/movie/sm1/')
+        result = json.loads(response.content.decode())
         # status must be OK
         assert response.status_code == 200
         # not return non-indexed movie
-        assert response.context['movie'].id == 'sm1'
-
-    def test_tags(self):
-        sch().create_testcases(dict(
-            mvid='sm1',
-            postdate=self.now - timedelta(days=8),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,0,0,0,0,0,0,0),
-        ))
-        response = self.c.get('/movie/sm1/')
-        # status must be OK
-        assert response.status_code == 200
-        # not return non-indexed movie
-        assert 'にこにこ' in list(map(lambda x: getattr(x, 'tagname'), response.context['tags'] ))
-        assert '初音ミク' in list(map(lambda x: getattr(x, 'tagname'), response.context['tags'] ))
-
-    def test_related(self):
-        sch().create_testcases(dict(
-            mvid='sm1',
-            postdate=self.now - timedelta(days=8),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,0,0,0,0,0,0,0),
-        ),dict(
-            mvid='sm2',
-            postdate=self.now - timedelta(days=7),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,1,0,0,0,0,0,0),
-        ),dict(
-            mvid='sm3',
-            postdate=self.now - timedelta(days=6),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,1,1,0,0,0,0,0),
-        ),dict(
-            mvid='sm4',
-            postdate=self.now - timedelta(days=5),
-            validity=True,
-            max_view=90,
-            tags=['にこにこ', '初音ミク'],
-            index=(1,1,1,0,0,0,0,0),
-        ))
-        response = self.c.get('/movie/sm1/')
-        # status must be OK
-        assert response.status_code == 200
-        # not return non-indexed movie
-        assert list(map(lambda x: getattr(x, 'destination'), response.context['related'] )) == ['sm2', 'sm3', 'sm4']
+        assert result['id'] == 'sm1'
 
     def test_404(self):
-        response = self.c.get('/movie/sm1/')
+        response = self.c.get('/api/movie/sm1/')
         assert response.status_code == 404
-
-    def test_redirect(self):
-        response = self.c.get('/movie/detail?movie_id=sm1')
-        assert response.status_code == 301

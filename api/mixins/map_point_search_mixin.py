8967eb29041721293686a6392d6ea11bf09b611c
Nanamachi <7machi@nanamachi.net>
2018-12-20 13:52:22 +0900

    change MapPointSearchMixIn: Distance can watch score and time

--- api/mixins/map_point_search_mixin.py
+++ api/mixins/map_point_search_mixin.py
@@ -1,10 +1,17 @@
 import json
 
-from django.db.models import F
+from django.db.models import F, Func, FloatField, ExpressionWrapper
+from django.db.models.functions import Now
 from rest_framework.exceptions import ValidationError
 
 from .base_map_search_mixin import BaseMapSearchMixIn
 from .common import *
+
+class Log10(Func):
+    function='LOG'
+
+class Sqrt(Func):
+    function='SQRT'
 
 class MapPointSearchMixIn(BaseMapSearchMixIn):
     def get_context_from_request(self, request):
@@ -16,6 +23,16 @@ class MapPointSearchMixIn(BaseMapSearchMixIn):
         else:
             raise ValidationError(detail='parameter "origin" is not valid')
 
+        if get_request('score_factor') not in ['', None]:
+            context['score_factor'] = float(get_request('score_factor')) / 10
+        else:
+            context['score_factor'] = float(0)
+
+        if get_request('time_factor') not in ['', None]:
+            context['time_factor'] = float(get_request('time_factor')) / 10
+        else:
+            context['time_factor'] = float(0)
+
         return context
 
     def _get_queryset(self, objects, context):
@@ -24,16 +41,23 @@ class MapPointSearchMixIn(BaseMapSearchMixIn):
         objects = objects.filter( **condition )
         objects = super()._get_queryset(objects, context)
 
-        objects = objects.annotate(
-            distance=\
-                ( F('songindex__value0') - pos[0] ) ** 2 \
-                + ( F('songindex__value1') - pos[1] ) ** 2 \
-                + ( F('songindex__value2') - pos[2] ) ** 2 \
-                + ( F('songindex__value3') - pos[3] ) ** 2 \
-                + ( F('songindex__value4') - pos[4] ) ** 2 \
-                + ( F('songindex__value5') - pos[5] ) ** 2 \
-                + ( F('songindex__value6') - pos[6] ) ** 2 \
-                + ( F('songindex__value7') - pos[7] ) ** 2 \
+        objects = objects.filter(
+            score__isnull=False
+        ).annotate(
+            distance=Sqrt(( F('songindex__value0') - pos[0] ) ** 2
+                + ( F('songindex__value1') - pos[1] ) ** 2
+                + ( F('songindex__value2') - pos[2] ) ** 2
+                + ( F('songindex__value3') - pos[3] ) ** 2
+                + ( F('songindex__value4') - pos[4] ) ** 2
+                + ( F('songindex__value5') - pos[5] ) ** 2
+                + ( F('songindex__value6') - pos[6] ) ** 2
+                + ( F('songindex__value7') - pos[7] ) ** 2
+            ) - context['score_factor'] * Log10( F('score') + 1 )
+            + context['time_factor'] * (
+                Log10( ExpressionWrapper(
+                    Now() - F('postdate'),
+                    output_field=FloatField()
+                )) - 11.0 ),
         ).order_by('distance')
 
         return objects
